58af69407c1144ad650bb4f258beba58553a4556
Cristian Maglie <c.maglie@bug.st>
2022-08-01 11:22:38 +0200

    Added license

---
+++ doc.go
@@ -0,0 +1,10 @@
+//
+// Copyright 2022 Cristian Maglie. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+/*
+Package testsuite is a collection of utilities for integration/unit testing in golang.
+*/
+package testsuite
